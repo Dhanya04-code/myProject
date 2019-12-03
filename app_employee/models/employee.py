f975687176aee0c882075d246df9ca2b162efd0c
Neenu <neenu.nandanan@mdrift.in>
2019-12-03 10:42:22 +0530

    employee id changed to text

--- app_employee/models/employee.py
+++ app_employee/models/employee.py
@@ -3,5 +3,5 @@ from django.db import models
 class Employee(models.Model):
     firstname = models.TextField()
     lastname = models.TextField()
-    emp_id = models.IntegerField()
+    emp_id = models.TextField()
     is_active = models.BooleanField(default=True)
